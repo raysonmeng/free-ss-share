9f4d4fc294a1e7f98e2dd04aab8f2ee78eccae71
raysonmeng <rayson951005@gmail.com>
2017-05-29 23:21:25 +0800

    fix ascii to str

--- free_ss_share/db/redis_model.py
+++ free_ss_share/db/redis_model.py
@@ -12,7 +12,7 @@ def save(account_list):
         redis.hset(account['ip'], 'location', account['location'])
         redis.hset(account['ip'], 'type', account['type'])
         redis.hset(account['ip'], 'link', account['link'])
-        print '+    Eat {ip} {location}'.format(ip=account['ip'].decode('utf-8'), location=account['location'].decode('utf-8'))
+        print '+    Eat ', '{ip} {location}'.format(ip=account['ip'], location=account['location'])
 
 
 def get():
