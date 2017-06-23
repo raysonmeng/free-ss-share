ff610b35a7c522c87d004ea8c96dd43a2648e0a2
raysonmeng <rayson951005@gmail.com>
2017-06-24 00:46:46 +0800

    last version before dabian

--- free_ss_share/spider/king_spider.py
+++ free_ss_share/spider/king_spider.py
@@ -8,8 +8,8 @@ from free_ss_share.spider.guagua_spider import guagua_get_account
 from free_ss_share.utils.ss_link import clean_link
 from free_ss_share.utils.ss_speed_test import test
 
-
 bowl = []
+
 
 class SpiderThread(threading.Thread):
     def __init__(self, spider):
@@ -17,12 +17,40 @@ class SpiderThread(threading.Thread):
         self.name = spider['name']
         self.func = spider['func']
 
-
     def run(self):
         print('|    ' + self.name + ' 爬虫启动成功！开始捕食，请稍等......')
         meal = self.func()
         bowl.extend(meal)
         print('|    ' + self.name + ' 捕食成功，共获取到 {num} 条账号信息！'.format(num=str(len(meal))))
+
+
+def chew_meal():
+    print '+                                                                   +'
+    print '+-----------------------STRAT CHEW MEAL!----------------------------+'
+    for account in bowl:
+        if 'ss:' in account['link']:
+            account['type'] = 'Shadowsocks'
+        else:
+            account['type'] = 'ShadowsocksR'
+        print '|'
+        print '|----开始评分系统，请稍等。。。-------------------------------------+'
+        try:
+            account['score'] = test(account['link'])
+        except Exception:
+            print '|----评分系统结束，评分失败，舍弃本条数据。-------------------------------+'
+            account['score'] = 0.0
+            continue
+
+        print '|----评分系统结束，最终得分{score}。------------------------------------+'.format(score=str(account['score']))
+        account['link'] = clean_link(account['link'])
+        time.sleep(5)
+    print '+-----------------------FINISH CHEW MEAL!---------------------------+'
+    print '+                                                                   +'
+
+
+def eat_meal():
+    save(bowl)
+
 
 class KingSpider(object):
     def __init__(self):
@@ -31,11 +59,6 @@ class KingSpider(object):
             {
                 'name': 'Doubi',
                 'func': doub_get_account
-            },
-
-            {
-                'name': 'Guagua',
-                'func': guagua_get_account
             }
         ]
 
@@ -56,32 +79,6 @@ class KingSpider(object):
         print '+-----------------------FINISH GET MEAL!----------------------------+'
         print '+                                                                   +'
 
-    def chew_meal(self):
-        print '+                                                                   +'
-        print '+-----------------------STRAT CHEW MEAL!----------------------------+'
-        for account in bowl:
-            if 'ss:' in account['link']:
-                account['type'] = 'Shadowsocks'
-            else:
-                account['type'] = 'ShadowsocksR'
-            print '|'
-            print '|----开始评分系统，请稍等。。。-------------------------------------+'
-            try:
-                account['score'] = test(account['link'])  # 加这一步每个代理需要多耗时10s
-            except Exception:
-                print '|----评分系统结束，评分失败，舍弃本条数据。------------------------------------+'
-                account['score'] = 0.0
-                continue
-
-            print '|----评分系统结束，最终得分{score}。------------------------------------+'.format(score=str(account['score']))
-            account['link'] = clean_link(account['link'])
-            time.sleep(5)
-        print '+-----------------------FINISH CHEW MEAL!---------------------------+'
-        print '+                                                                   +'
-
-    def eat_meal(self):
-        save(bowl)
-
 
 # run spider
 def run_spider():
@@ -92,12 +89,14 @@ def run_spider():
         king.get_meal()
         if not bowl:
             break
-        king.chew_meal()
-        king.eat_meal()
+        chew_meal()
+        eat_meal()
         bowl = []
         sleep_time = random.randint(1, 60)
-        print('\n|    Sleep {sleep_time} second!  _(:3」∠)_ 本次用时{use_time}\n'.format(sleep_time=sleep_time,use_time=time.time()-start_time))
+        print('\n|    Sleep {sleep_time} second!  _(:3」∠)_ 本次用时{use_time}\n'.format(sleep_time=sleep_time,
+                                                                                    use_time=time.time() - start_time))
         time.sleep(sleep_time)
+
 
 if __name__ == '__main__':
     run_spider()
